80e4bd873e21e8dcafcf513c0c7692214780f0af
mohitsingh78 <156327092+mohitsingh78@users.noreply.github.com>
2024-01-13 12:41:55 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+
+var config = {
+    lines: [
+        "Hey Amayra,",
+"Jab se tu mere zindagi mein aayi hai, har din ek naya adventure ho gaya hai. Tere bina toh meri life ek dull movie ki tarah lagti thi, par ab toh yeh ek full-on Bollywood blockbuster hai!",
+"Tere saath har cheez exciting ho gayi hai. Tere saath fight karne ka mazaa hi kuch aur hai, kyunki phir baad mein manana bhi toh ek alag hi level ka entertainment hai.",
+"Tere hasi se meri saari tension door ho jaati hai. Kabhi sochta hoon, agar tere jokes ka royalty milta, toh ab tak main ek Lamborghini mein ghoom raha hota.",
+"Amayra, tu meri life ka Netflix hai, jise main hamesha binge-watch karna chahta hoon. Tere bina life adhoori si lagti hai, jaise pizza without cheese!",
+"Toh yeh tha mera attempt at a unique and funny love letter. Tumhare bina life ka maza hi kuch aur hai. Love you, Amayra!",
+"Mazak mein bhi, tere saath hamesha,"
+
+       
+    ],
+    date:"2023-05-22T20:15:00",
+    names: ["☃︎【 Your bbe 】","‎ "]
+}
